56ec16512daf90c59f0c3d040bbe11d6c2b1e7bc
Ayush Sahu <ayushsahu871@gmail.com>
2026-08-27 19:01:25 +0530

    Configure Vercel deployment, CORS, root gitignore and start scripts

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.post(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, "http://localhost:5173"] : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
